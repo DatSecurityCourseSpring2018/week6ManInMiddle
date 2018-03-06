b5153fe26b131590e26ba77ff0e3fd9e53a65a01
Kasper Osterbye <kasper@cphbusiness.dk>
2018-03-06 16:43:59 +0100

    Final commit

--- src/main/java/cphkasper/rsa/Main.java
+++ src/main/java/cphkasper/rsa/Main.java
@@ -12,9 +12,40 @@ public class Main {
 
     public static void main( String[] args ) throws IOException {
         // Setup
-        RSAKeyPair pair = new RSAKeyPair();
-        System.out.println( "RSA: " + pair );
+        RSAKeyPair myPair = new RSAKeyPair();
+        System.out.println( "RSA: " + myPair );
+        RSAKeyPair other = new RSAKeyPair();
 
+        //SignMessage(myPair);
+        //EndcryptMessage(pair);
+        
+        String msg = "Iam really me";
+        String enc = myPair.encrypt( msg);
+        System.out.println( "Encoded: " + enc);
+        enc = other.getPublicKey().encrypt( enc);
+        
+        // Send enc to the other....
+        String dec = other.decrypt( enc);
+        dec = myPair.getPublicKey().decrypt( dec);
+        
+        System.out.println( "Signed encrypted message was: " +dec );
+        
+        
+        
+
+    }
+
+    private static void SignMessage( RSAKeyPair pair ) {
+        String signature = "I am me";
+        String signedMessage = pair.encrypt( signature );
+        
+        // send to the world who has my public key
+        RSAKey world = pair.getPublicKey();
+        String message = world.decrypt( signedMessage);
+        System.out.println( "Public decrypted message: " + message );
+    }
+
+    private static void EndcryptMessage( RSAKeyPair pair ) {
         // Send public key to the world
         RSAKey publ = pair.getPublicKey();
 
@@ -30,6 +61,5 @@ public class Main {
         // encrypt
         String decrypted = pair.decrypt( encrypted );
         System.out.println( "Decrypted: " + decrypted );
-
     }
 }
